d54939deb286192729fc7df5106730db62525e26
dmitrybukhonov <d.buhonov@yandex.ru>
2024-03-18 18:24:32 +0300

    first commit

---
+++ migrations/m240318_152021_migration_name.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240318_152021_migration_name
+ */
+class m240318_152021_migration_name extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m240318_152021_migration_name cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m240318_152021_migration_name cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
